9e58eb9a29dcb238b78ee9306dca2c9608128526
rixx <git@foyfoy.de>
2014-04-13 13:27:38 +0200

    added comments to C file

--- trig.c
+++ trig.c
@@ -1,14 +1,20 @@
 #include<stdio.h>
 #include<math.h>
 
+/* as specified */
 float rixxsin(float);
 float rixxcos(float);
 float rixxtan(float);
 float rixxsin0(float);
 float rixxtan0(float);
 
+/* maximum number of Taylor series terms */
 int MAX_ITER = 5;
 
+/* as specified: user is prompted for x_min, x_max and n
+ * then for n equidistant values in [x_min, x_max], sin, cos and tan are printed
+ * bonus feature: in brackets, the deviation from the real value
+ */
 int main() {
     int n;
     float x_min, x_max, intervall, step;
@@ -24,11 +30,12 @@ int main() {
 
     intervall = (x_max - x_min) / (n - 1);
 
-    if (intervall < 0){
-        printf("x_min must be smaller than x_max!");
+    if (intervall < 0) {
+        printf("x_min must be smaller than x_max and n larger than 1!");
         return -1;
     }
 
+    /* start at x_min, do n steps */
     for (step = x_min; n > 0; n--, step += intervall) {
         float sinus, cosinus, tangens;
 
@@ -45,8 +52,9 @@ int main() {
     return 0;
 }
 
-
+/* takes any value x, normalizes into [-pi, pi], then calls rixxsin0 */
 float rixxsin(float x) {
+    /* normalizing */
     while (x < -M_PI) {
        x += 2*M_PI;
     }
@@ -55,6 +63,8 @@ float rixxsin(float x) {
        x -= 2*M_PI;
     }
 
+    /* calling rixxsin0. minor modifications, since rixxsin0 is defined
+     * for [-pi/2, pi/2] */
     if (x < -M_PI/2) {
         return -rixxsin0(x + M_PI);
     } else if (x > M_PI/2) {
@@ -64,6 +74,8 @@ float rixxsin(float x) {
     }
 }
 
+/* computes the taylor series for sin(x):
+ * sum((-1)^(n−1) * x^(2n - 1) / (2n - 1)!) */
 float rixxsin0(float x) {
     int i, fac = 1;
     float pow = x, result = x;
@@ -82,11 +94,13 @@ float rixxsin0(float x) {
     return result;
 }
 
-
+/* cos(x) = sin(pi/2 - x) */
 float rixxcos(float x) {
     return rixxsin(M_PI/2 - x);
 }
 
+/* for reasons, rixxtan is supposed to call rixxtan0, which may only take
+ * values in ]-pi/2, pi/2[ */
 float rixxtan(float x) {
     while (x < -M_PI/2) {
         x += M_PI;
@@ -97,12 +111,16 @@ float rixxtan(float x) {
     }
     
     if ((x == M_PI/2) || (x == -M_PI/2)) {
+        /* ok, this should be inf or nan, but wth … */
         return 2;
     } else {
         return rixxtan0(x);
     }
 }
 
+/* and this is just plain stupid -- rixxtan0 may only call rixxsin0,
+ * but since tan = sin/cos, this needs to re-implement part of the 
+ * rixxcos logic */
 float rixxtan0(float x) {
     float cosinus, sinus = rixxsin0(x);
     float cos_x = M_PI/2 - x;
