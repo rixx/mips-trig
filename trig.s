ed0b28821925f7c76d9893da068eef8334d70817
rixx <git@foyfoy.de>
2014-04-24 21:46:49 +0200

    make all the registers nicer

--- trig.s
+++ trig.s
@@ -34,36 +34,41 @@ main:
     li      $v0, 4              # load system code for string output
     syscall                     # print string
 
-    li      $v0, 5              # load system code for float input
-    syscall                     # read float into $f0
+    li      $v0, 5              # load system code for int input
+    syscall                     # read int into $v0
     move    $s0, $v0            # move steps to $s0
 
-    addi    $t1, $s0, -1        # write n-1 to $t1
-    blez    $t1, error1         # fail if steps <= 1
+    addi    $t0, $s0, -1        # write n-1 to $t0
+    blez    $t0, error1         # fail if steps <= 1
 
-    mtc1    $t1, $f4
+    mtc1    $t0, $f4
     cvt.s.w $f4, $f4            # cast n-1 to float (in $f4)
 
     sub.s   $f3, $f2, $f1       # write (xmax-xmin) to $f3
-    div.s   $f20, $f3, $f4      # intervall = (xmax-xmin)/(n-1) in $f20
+    div.s   $f21, $f3, $f4      # intervall = (xmax-xmin)/(n-1) in $f21
 
-    mov.s   $f12, $f1           # write x_min (steps) to $f12
+    mov.s   $f20, $f1           # write x_min (steps) to $f20
 
 main_loop:
+    mov.s   $f12, $f20          # move step to $f12
     jal     sin                 # call sin(step)
-    mov.s   $f21, $f0           # move sin(step) to $f21
+
+    mov.s   $f22, $f0           # move sin(step) to $f22
+    mov.s   $f12, $f20          # move step to $f12
     jal     cos                 # call cos(step)
-    mov.s   $f22, $f0           # move cos(step) to $f22
+
+    mov.s   $f23, $f0           # move cos(step) to $f23
+    mov.s   $f12, $f20          # move step to $f12
     jal     tan                 # call tan(step)
    
     addi    $s0, -1             # n--;
     beqz    $s0, end            # exit if n == 0
 
-    add.s   $f12, $f12, $f20    # step += intervall;
+    add.s   $f20, $f20, $f21    # step += intervall;
     j       main_loop           # looooop
 
 
-sin:
+sin
     subu    $sp, $sp, 4         # allocate space to save $ra and x
     sw      $ra, 4($sp)         # save $ra
     s.s     $f12, 0($sp)        # save x
